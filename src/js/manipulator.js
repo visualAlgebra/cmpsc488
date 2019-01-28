a9f7a8e6a7de59eda11e74241f9492cb594366f2
eignnx <eignnx@gmail.com>
2019-01-28 12:11:36 -0500

    Add manipulator and creator pages

---
+++ src/js/manipulator.js
@@ -0,0 +1,21 @@
+let canvasContainer;
+let cloudImg;
+
+function preload() {
+  cloudImg = loadImage('assets/cloud.svg');
+}
+
+function setup() {
+  canvasContainer = select('#canvas-container');
+  canvas = createCanvas(700, 500);
+  canvas.parent(canvasContainer);
+  background(51);
+
+  cloudImg.resize(300, 250);
+}
+
+function draw() {
+  fill(255);
+  imageMode(CENTER);
+  image(cloudImg, width/2, height/2);
+}
