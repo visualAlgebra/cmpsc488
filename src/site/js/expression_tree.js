a6b58425359dc41dd1afd48f56876477acc77525
eignnx <eignnx@gmail.com>
2019-02-08 18:02:33 -0500

    Migrate to dom rendering for expression trees

--- src/site/js/expression_tree.js
+++ src/site/js/expression_tree.js
@@ -1,425 +1,313 @@
 const Orientation = {
-  EW: 'eastwest',
-  NS: 'northsouth'
-}
+  EW: "eastwest",
+  NS: "northsouth"
+};
+
+String.prototype.splice = function(index, str) {
+  return this.slice(0, index) + str + this.slice(index);
+};
+
 // Abstract Class
 class ExpressionTree {
-  constructor (kind) {
-    this.kind = kind
-    this.treeCount = 1
-    this.parent = null
+  constructor(kind) {
+    this.kind = kind;
+    this.treeCount = 1;
+    this.parent = null;
   }
 
-  toString () {
-    return 'error'
+  toString() {
+    return "error";
   }
 } // end ExpressionTree class
 
 class Tag extends ExpressionTree {
-  constructor (orientation, nw, se) {
-    super('tag')
-    this.orientation = orientation
-    this.NW = nw || []
-    this.SE = se || []
+  constructor(orientation, nw, se) {
+    super("tag");
+    this.orientation = orientation;
+    this.NW = nw || [];
+    this.SE = se || [];
 
     for (let child of this.NW) {
-      child.parent = this
-      this.treeCount += child.treeCount
+      child.parent = this;
+      this.treeCount += child.treeCount;
     }
     for (let child of this.SE) {
-      child.parent = this
-      this.treeCount += child.treeCount
+      child.parent = this;
+      this.treeCount += child.treeCount;
     }
   }
 
-  updateParentTreeCount (count) {
-    this.treeCount += count
+  updateParentTreeCount(count) {
+    this.treeCount += count;
     if (this.parent != null) {
-      this.parent.updateParentTreeCount(1)
+      this.parent.updateParentTreeCount(1);
     }
   }
   //
-  addSouthEast (child) {
-    this.SE.push(child)
-    child.parent = this
-    this.updateParentTreeCount(1)
+  addSouthEast(child) {
+    this.SE.push(child);
+    child.parent = this;
+    this.updateParentTreeCount(1);
   }
 
-  addNorthWest (child) {
-    this.NW.push(child)
-    child.parent = this
-    this.updateParentTreeCount(1)
+  addNorthWest(child) {
+    this.NW.push(child);
+    child.parent = this;
+    this.updateParentTreeCount(1);
   }
 
-  equals (that) {
-    if (that.kind === 'tag') {
+  equals(that) {
+    if (that.kind === "tag") {
       if (
         this.orientation !== that.orientation ||
         this.NW.length !== that.NW.length ||
         this.SE.length !== that.SE.length
       ) {
-        return false
+        return false;
       }
 
       for (let i = 0; i < this.NW.length; i++) {
-        if (!this.NW[i].equals(that.NW[i])) return false
+        if (!this.NW[i].equals(that.NW[i])) return false;
       }
 
       for (let i = 0; i < this.SE.length; i++) {
-        if (!this.SE[i].equals(that.SE[i])) return false
+        if (!this.SE[i].equals(that.SE[i])) return false;
       }
 
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 
-  delete (ref) {
-    array_delete(this.SE, ref)
-    array_delete(this.NW, ref)
+  delete(ref) {
+    array_delete(this.SE, ref);
+    array_delete(this.NW, ref);
   }
 
-  /**
-   * Renders to a p5.js canvas.
-   * @param {p5 sketch instance} p
-   */
-  render (p) {
-    p.push()
-    switch (this.orientation) {
-      case Orientation.EW: {
-        let leftWidth = 0
-        p.push()
-        for (let i = this.NW.length - 1; i >= 0; i--) {
-          let child = this.NW[i]
-          const childWidth = child.dimensions().width
-          p.translate(-childWidth / 2, 0)
-          child.render(p)
-          p.translate(-childWidth / 2, 0)
-          leftWidth += childWidth
-        }
-        p.pop()
+  // Creates dom elements for the tag, returns dom node without putting
+  // it on the page.
+  render() {
+    const div = document.createElement("div");
+    div.classList.add(
+      this.orientation.splice(this.orientation.length / 2, "-")
+    );
 
-        let rightWidth = 0
-        p.push()
-        for (let i = 0; i < this.SE.length; i++) {
-          let child = this.SE[i]
-          const childWidth = child.dimensions().width
-          p.translate(childWidth / 2, 0)
-          child.render(p)
-          p.translate(childWidth / 2, 0)
-          rightWidth += childWidth
-        }
-        p.pop()
+    div.classList.add("tag");
+    div.setAttribute("data-id", this);
 
-        const height = this.dimensions().height
-        p.noFill()
-        p.strokeWeight(5)
-        p.stroke(0, 0, 255)
-        p.rect(-leftWidth, -height / 2, leftWidth, height)
-        p.stroke(255, 0, 0)
-        p.rect(0, -height / 2, rightWidth, height)
-        break
-      }
+    const nw = document.createElement("div");
 
-      case Orientation.NS: {
-        let topHeight = 0
-        p.push()
-        for (let i = this.NW.length - 1; i >= 0; i--) {
-          let child = this.NW[i]
-          const childHeight = child.dimensions().height
-          p.translate(0, -childHeight / 2)
-          child.render(p)
-          p.translate(0, -childHeight / 2)
-          topHeight += childHeight
-        }
-        p.pop()
+    nw.className = "north-west";
 
-        let bottomHeight = 0
-        p.push()
-        for (let i = this.SE.length - 1; i >= 0; i--) {
-          let child = this.SE[i]
-          const childHeight = child.dimensions().height
-          p.translate(0, childHeight / 2)
-          child.render(p)
-          p.translate(0, childHeight / 2)
-          bottomHeight += childHeight
-        }
-        p.pop()
+    const se = document.createElement("div");
 
-        const width = this.dimensions().width
-        p.noFill()
-        p.strokeWeight(5)
-        p.stroke(0, 0, 255)
-        p.rect(-width / 2, -topHeight, width, topHeight)
-        p.stroke(255, 0, 0)
-        p.rect(-width / 2, 0, width, bottomHeight)
-        break
-      }
+    se.className = "south-east";
 
-      default:
-        break
-    }
-    p.pop()
+    this.NW.forEach(child => {
+      nw.appendChild(child.render());
+    });
+
+    this.SE.forEach(child => {
+      se.appendChild(child.render());
+    });
+
+    div.appendChild(nw);
+    div.appendChild(se);
+    return div;
   }
 
-  dimensions () {
-    switch (this.orientation) {
-      case Orientation.EW: {
-        const width =
-          this.NW.reduce((acc, child) => acc + child.dimensions().width, 0) +
-          this.SE.reduce((acc, child) => acc + child.dimensions().width, 0) +
-          10
-        const height =
-          Math.max(
-            this.NW.reduce(
-              (acc, child) => Math.max(acc, child.dimensions().height),
-              0
-            ),
-            this.SE.reduce(
-              (acc, child) => Math.max(acc, child.dimensions().height),
-              0
-            )
-          ) + 10
-        return { width, height }
-      }
-
-      case Orientation.NS: {
-        const width =
-          Math.max(
-            this.NW.reduce(
-              (acc, child) => Math.max(acc, child.dimensions().width),
-              0
-            ),
-            this.SE.reduce(
-              (acc, child) => Math.max(acc, child.dimensions().width),
-              0
-            )
-          ) + 10
-        const height =
-          this.NW.reduce((acc, child) => acc + child.dimensions().height, 0) +
-          this.SE.reduce((acc, child) => acc + child.dimensions().height, 0) +
-          10
-        return { width, height }
-      }
-
-      default:
-        break
-    }
-  }
-
-  toString () {
+  toString() {
     var retval =
-      '{t' +
+      "{t" +
       Object.keys(Orientation).find(
         key => Orientation[key] === this.orientation
       ) +
-      '{{'
+      "{{";
     for (let i = 0; i < this.NW.length; i++) {
-      retval = retval + this.NW[i].toString()
+      retval = retval + this.NW[i].toString();
     }
-    retval = retval + '}{'
+    retval = retval + "}{";
     for (let i = 0; i < this.SE.length; i++) {
-      retval = retval + this.SE[i].toString()
+      retval = retval + this.SE[i].toString();
     }
-    return retval + '}}}'
+    return retval + "}}}";
   }
 } // end Tag class
 
 class Variable extends ExpressionTree {
-  constructor (value) {
-    super('variable')
-    this.value = value
+  constructor(value) {
+    super("variable");
+    this.value = value;
   }
 
-  equals (that) {
-    if (that.kind !== 'variable') return false
-    return this.value === that.value
+  equals(that) {
+    if (that.kind !== "variable") return false;
+    return this.value === that.value;
   }
 
-  /**
-   * Renders to a p5.js canvas.
-   * @param {p5 sketch instance} p
-   */
-  render (p) {
-    p.noStroke()
-    p.fill(200, 0, 200)
-    p.ellipse(0, 0, 50, 50)
-    p.fill(255)
-    p.textSize(30)
-    p.text(`x${this.value}`, -15, 15)
+  // Creates dom elements for the tag, returns dom node without putting
+  // it on the page.
+  render() {
+    const div = document.createElement("div");
+    div.textContent = `x${this.value}`;
+    div.className = "variable";
+    div.setAttribute("data-id", this);
+    return div;
   }
 
-  dimensions () {
-    return {
-      width: 100,
-      height: 100
-    }
-  }
-
-  toString () {
-    return '{v' + this.value + '}'
+  toString() {
+    return "{v" + this.value + "}";
   }
 } // end Variable class
 
 class Literal extends ExpressionTree {
-  constructor (value) {
-    super('literal')
-    this.value = value
+  constructor(value) {
+    super("literal");
+    this.value = value;
   }
 
-  equals (that) {
-    if (that.kind !== 'literal') return false
-    return this.value === that.value
+  equals(that) {
+    if (that.kind !== "literal") return false;
+    return this.value === that.value;
   }
 
-  /**
-   * Renders to a p5.js canvas.
-   * @param {p5 sketch instance} p
-   */
-  render (p) {
-    p.noStroke()
-    p.fill(0, 200, 100)
-    p.rectMode(p.CENTER)
-    p.rect(0, 0, 50, 50)
-    p.fill(255)
-    p.textSize(30)
-    p.text(this.value, -15, 15)
+  // Creates dom elements for the tag, returns dom node without putting
+  // it on the page.
+  render() {
+    const div = document.createElement("div");
+    div.textContent = this.value;
+    div.className = "literal";
+    div.setAttribute("data-id", this);
+    return div;
   }
 
-  dimensions () {
-    return {
-      width: 100,
-      height: 100
-    }
-  }
-
-  toString () {
-    return '{l' + this.value + '}'
+  toString() {
+    return "{l" + this.value + "}";
   }
 } // end Literal class
 
 // end of classes, start of functions
 
-function array_delete (arr, ref) {
+function array_delete(arr, ref) {
   for (let i = 0; i < arr.length; i++) {
     if (Object.is(ref, arr[i])) {
-      arr.splice(i, 1)
+      arr.splice(i, 1);
     }
   }
 }
 
-function Deserialize (text) {
+function Deserialize(text) {
   // eric
-  if (text.substr(0, 2) === '{l') {
-    return new Literal(parseInt(text.substr(2, text.length - 3)))
+  if (text.substr(0, 2) === "{l") {
+    return new Literal(parseInt(text.substr(2, text.length - 3)));
   }
-  if (text.substr(0, 2) === '{v') {
-    return new Variable(parseInt(text.substr(2, text.length - 3)))
+  if (text.substr(0, 2) === "{v") {
+    return new Variable(parseInt(text.substr(2, text.length - 3)));
   }
-  if (text.substr(0, 2) === '{t') {
-    let orient = Orientation[text.substr(2, 2)]
-    let retval = new Tag(orient)
-    let firstindex = text.indexOf('{{') + 1
-    let lastindex = text.length - 2
+  if (text.substr(0, 2) === "{t") {
+    let orient = Orientation[text.substr(2, 2)];
+    let retval = new Tag(orient);
+    let firstindex = text.indexOf("{{") + 1;
+    let lastindex = text.length - 2;
     // helper(text.substr(firstindex,lastindex-firstindex));
-    let midindex = 0
-    let counter = 0
+    let midindex = 0;
+    let counter = 0;
     for (let i = firstindex; i < text.length; i++) {
-      if (text.charAt(i) === '{') {
-        counter++
-      } else if (text.charAt(i) === '}') {
-        counter--
+      if (text.charAt(i) === "{") {
+        counter++;
+      } else if (text.charAt(i) === "}") {
+        counter--;
         if (counter === 0) {
-          midindex = i
-          break
+          midindex = i;
+          break;
         }
       }
     }
-    let t1 = text.substr(firstindex, midindex - firstindex + 1)
+    let t1 = text.substr(firstindex, midindex - firstindex + 1);
     // helper(t1);
-    let tempstr = ''
-    if (t1 !== '{}') {
+    let tempstr = "";
+    if (t1 !== "{}") {
       for (let i = firstindex + 1; i < midindex; i++) {
-        tempstr = tempstr + text.charAt(i)
-        if (text.charAt(i) === '{') {
-          counter++
-        } else if (text.charAt(i) === '}') {
-          counter--
+        tempstr = tempstr + text.charAt(i);
+        if (text.charAt(i) === "{") {
+          counter++;
+        } else if (text.charAt(i) === "}") {
+          counter--;
           if (counter === 0) {
             // helper(tempstr);
-            let d = Deserialize(tempstr)
-            retval.addNorthWest(d)
-            tempstr = ''
+            let d = Deserialize(tempstr);
+            retval.addNorthWest(d);
+            tempstr = "";
           }
         }
       }
     }
-    let t2 = text.substr(midindex + 1, lastindex - midindex - 1)
+    let t2 = text.substr(midindex + 1, lastindex - midindex - 1);
     // helper(t2);
-    tempstr = ''
-    if (t2 !== '{}') {
+    tempstr = "";
+    if (t2 !== "{}") {
       for (let i = midindex + 2; i < lastindex - 1; i++) {
-        tempstr = tempstr + text.charAt(i)
-        if (text.charAt(i) === '{') {
-          counter++
-        } else if (text.charAt(i) === '}') {
-          counter--
+        tempstr = tempstr + text.charAt(i);
+        if (text.charAt(i) === "{") {
+          counter++;
+        } else if (text.charAt(i) === "}") {
+          counter--;
           if (counter === 0) {
             // helper(tempstr);
-            let d = Deserialize(tempstr)
-            retval.addSouthEast(d)
-            tempstr = ''
+            let d = Deserialize(tempstr);
+            retval.addSouthEast(d);
+            tempstr = "";
           }
         }
       }
     }
-    return retval
+    return retval;
   }
 }
 
-function compress_string_js (text, callback) {
-  var arr
+function compress_string_js(text, callback) {
+  var arr;
   /// If the string is a JSON array, use that. This allows us to compress a byte array.
-  if (text[0] === '[' && text.slice(-1) === ']') {
+  if (text[0] === "[" && text.slice(-1) === "]") {
     try {
-      arr = JSON.parse(text)
+      arr = JSON.parse(text);
     } catch (e) {}
   }
   if (arr) {
-    text = arr
+    text = arr;
   }
-  LZMA('src/site/js/lzma_worker.js').compress(text, 9, callback)
+  LZMA("src/site/js/lzma_worker.js").compress(text, 9, callback);
 }
 
-function decompress_string_js (byte_arr, callback) {
-  LZMA('src/site/js/lzma_worker.js').decompress(byte_arr, callback)
+function decompress_string_js(byte_arr, callback) {
+  LZMA("src/site/js/lzma_worker.js").decompress(byte_arr, callback);
 }
 
 /// ///////////////////////////////////////////////////////////////////
 /// //////////////////        Unused Functions   //////////////////////
 
-function helper (text) {
-  let rettext = ''
-  let temp = 0
-  let zeroamt = 0
-  let i = 0
+function helper(text) {
+  let rettext = "";
+  let temp = 0;
+  let zeroamt = 0;
+  let i = 0;
   for (i = 0; i < text.length; i++) {
-    if (text.charAt(i) === '{') {
-      temp++
-      rettext = rettext + temp.toString()
-    } else if (text.charAt(i) === '}') {
-      rettext = rettext + temp.toString()
-      temp--
+    if (text.charAt(i) === "{") {
+      temp++;
+      rettext = rettext + temp.toString();
+    } else if (text.charAt(i) === "}") {
+      rettext = rettext + temp.toString();
+      temp--;
       if (temp === 0) {
-        zeroamt++
+        zeroamt++;
       }
     } else {
-      rettext = rettext + ' '
+      rettext = rettext + " ";
     }
   }
-  console.log(text)
-  console.log(rettext + '  :  ' + (temp === 0) + '  :  ' + zeroamt)
+  console.log(text);
+  console.log(rettext + "  :  " + (temp === 0) + "  :  " + zeroamt);
 }
 
 /// /////////////////////////////////////////////////////////////////////////////
--- src/site/js/manipulator.js
+++ src/site/js/manipulator.js
@@ -1,26 +1,20 @@
-let expressionTree = new Tag(Orientation.EW,[
+let expressionTree = new Tag(
+  Orientation.EW,
+  [
     new Literal(2),
-    new Tag(Orientation.NS,[new Literal(1), new Variable(1)], [new Literal(0)]),
+    new Tag(
+      Orientation.NS,
+      [new Literal(1), new Variable(1)],
+      [new Literal(0)]
+    ),
     new Variable(2)
   ],
   [new Variable(3), new Literal(0)]
 );
 
-function sketch(p) {
+// let expressionTree = new Literal(1);
 
-  p.preload = () => {
-  }
-
-  p.setup = () => {
-    p.createCanvas(700, 500);
-    p.background(51);
-    p.smooth();
-  }
-
-  p.draw = () => {
-    p.translate(p.width / 2, p.height / 2);
-    expressionTree.render(p);
-  }
-}
-
-let p5Sketch = new p5(sketch, 'canvas-container');
+window.onload = () => {
+  const container = document.getElementById("canvas-container");
+  container.appendChild(expressionTree.render());
+};
