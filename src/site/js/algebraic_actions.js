8a089055ff59697ca561f709bc3b0f35ea272cd0
Toan Nguyen <toikhongnho147@gmail.com>
2019-02-10 17:53:48 -0500

    Initial AssociativeIntro merge

--- src/site/js/algebraic_actions.js
+++ src/site/js/algebraic_actions.js
@@ -1,11 +1,8 @@
-
-
 // USES OF ALGEBRAIC ACTIONS
 // UI
 // AI
 // History Bar?
 //   - Might just save instances of previous
-
 
 // USE CASE FOR UI
 // User specify that a swap needs to happen between siblings A and B.
@@ -21,14 +18,13 @@
 // ...
 // If that was a bad choice, undo the algebraic action.
 
-
 // 1 + 2 => 2 + 1
 // 1 * 2 => 2 * 1
-//Takes in two siblings and a quadrant(the list where the siblings are located).
-//If the siblings and quadrant are valid, then the siblings will be swapped.
+// Takes in two siblings and a quadrant(the list where the siblings are located).
+// If the siblings and quadrant are valid, then the siblings will be swapped.
 class CommutativeSwap {
 
-  constructor(sibling1, sibling2, quadrant) {
+  constructor (sibling1, sibling2, quadrant) {
     this.sibling1 = sibling1;
     this.sibling2 = sibling2;
     this.quadrant = quadrant;
@@ -36,13 +32,13 @@ class CommutativeSwap {
 
   //verifys if the arguments are valid by checking
   //if the Siblings are in the same quadrant, then return true
-  verify() {
+  verify () {
     return quadrant.includes(sibling2)
         && quadrant.includes(sibling1);
   }
 
   //
-  apply(exprTree) {
+  apply (exprTree) {
 
     //Finds the index of the two siblings
     idx1 = quadrant.findIndex(x => x === sibling1);
@@ -74,6 +70,8 @@ class CommutativeSwap {
 }
 
 // (1 + 2) + 1 => 1 + 2 + 1
+//Takes two tags of the same orientation and one is in the other.
+//The inner tag is collapsed into the outer tag
 class AssociativeMerge {
   constructor(sibling, parent, quadrant) {
     this.sibling = sibling;
@@ -110,24 +108,51 @@ class AssociativeMerge {
     if(quadrant.equals(parent.NW)) {
       return new Tag(parent.orientation, newQuadrant, sibling.parent.SE);
     } else {
-      return new Tag(sibling1.parent.orientation, sibling.parent.NW, newQuadrant);
+      return new Tag(parent.orientation, sibling.parent.NW, newQuadrant);
     }
   }
 }
 
+//Takes an array of siblings in the same tag and surrounds those
+//siblings with a tag of the same orientation
 class AssociativeIntro {
-  constructor(sibling1, sibling2) {
-    this.sibling1 = sibling1;
-    this.sibling2 = sibling2;
+
+  constructor(siblings, parent, quadrant) {
+    this.siblings = siblings;
+    this.parent = parent;
+    this.quadrant = quadrant;
   }
 
+  //Valid if siblings is in parent in the correct order
   verify() {
-    // return this.sibling1 and this.sibling2 are actually siblings;
+
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
+
+    //make the inner tag
+    if (quadrant.equals(parent.NW)){
+      var newTag = new Tag(parent.orientation, siblings);
+    } else {
+      var newTag = new Tag(parent.orientation, [], siblings);
+    }
+
+    //find the index of the first sibling
+    idx = quadrant.findIndex(x => x.equals(sibling[0]));
+
+    newQuadrant = [];
+    for (var i = 0; i < quadrant.length - siblings.length; i++) {
+      if(i === idx) {
+        newQuadrant[i] = newTag;
+      }
+      newQuadrant[i] = quadrant[i];
+    }
+
+    if(quadrant.equals(parent.NW)) {
+      return new Tag(parent.orientation, newQuadrant, sibling.parent.SE);
+    } else {
+      return new Tag(parent.orientation, sibling.parent.NW, newQuadrant);
+    }
   }
 }
 
@@ -142,8 +167,7 @@ class Distribute {
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
+
   }
 }
 
@@ -158,8 +182,6 @@ class Factor {
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
   }
 }
 
@@ -174,8 +196,6 @@ class SplitFrac {
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
   }
 }
 
@@ -190,8 +210,6 @@ class CombineFrac {
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
   }
 }
 
@@ -206,8 +224,6 @@ class Cancel {
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
   }
 }
 
@@ -221,7 +237,6 @@ class IdentityBalance {
   }
 
   apply(exprTree) {
-    exprTree.find();
-    return new tree;
+
   }
 }
