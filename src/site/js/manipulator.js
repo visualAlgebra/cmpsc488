969889e960d134942c4ac1019f0dece1b5b06ff2
Fractalyst <fractalyst97@gmail.com>
2019-02-15 14:11:31 -0500

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 7e19cb5b9d995746dbd17023471a859782d945fe.

--- src/site/js/manipulator.js
+++ src/site/js/manipulator.js
@@ -2,11 +2,11 @@
 var problem_to_load=getProblemFromURL();
 
 window.onload = () => {
-    get_problem_from_db("TEST_PROBLEM_2", res => {
-        decompress_string_js(res.expression_start,decomp => {
-            document.getElementById("canvas-container").appendChild(Deserialize(decomp).render());
-        });
     });
+        });
+            document.getElementById("canvas-container").appendChild(Deserialize(decomp).render());
+        decompress_string_js(res.expression_start,decomp => {
+    get_problem_from_db("TEST_PROBLEM_2", res => {
 };
 
 function getProblemFromURL(){
