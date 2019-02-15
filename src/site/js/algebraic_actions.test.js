f1c44d716b5cc208f97c0b238813e02e514abb9d
Toan Nguyen <toikhongnho147@gmail.com>
2019-02-15 14:24:35 -0500

    Added unit tests for Factor and Distribute, added a negative function for convinience.

--- src/site/js/algebraic_actions.test.js
+++ src/site/js/algebraic_actions.test.js
@@ -6,6 +6,11 @@ function assertAA (applied, expected, err) {
   return retval;
 }
 
+function negative(x) {
+  x = new Tag(Orientation.EW, [],[x]);
+  return x;
+}
+
 function CSTest1 () {
   let a = new Tag(Orientation.NS);
   a0 = new Literal(1);
@@ -251,7 +256,7 @@ function AETest1() {
 
   const expected = new Tag(Orientation.NS,
     [
-      new Variable(1),
+      x,
       new Tag(Orientation.NS,
         [new Variable(2)],
         [new Variable(3)]
@@ -263,7 +268,7 @@ function AETest1() {
   const action = new AssociativeExtract(x, Quadrant.NW);
   action.apply()
 
-  assertAA(p, expected, "AssociativeExtract Test 1 failed");
+  return assertAA(p, expected, "AssociativeExtract Test 1 failed");
 }
 
 function AETest2() {
@@ -302,6 +307,185 @@ function AETest2() {
   return assertAA(p, expectedParent, "AssociativeExtract test 2 failed");
 }
 
+function AInsTest1() {
+  const x = new Variable(1);
+
+  const p = new Tag(Orientation.NS,
+    [
+      x,
+      new Tag(Orientation.NS,
+        [new Variable(2)],
+        [new Variable(3)]
+      )
+    ],
+    []
+  );
+
+  const expected = new Tag(Orientation.NS,
+    [
+      new Tag(Orientation.NS,
+        [x, new Variable(2)],
+        [new Variable(3)]
+      )
+    ],
+    []
+  );
+
+  const action = new AssociativeExtract(x, Quadrant.NW);
+  action.apply()
+
+  return assertAA(p, expected, "AssociativeInsert Test 1 failed");
+}
+
+function AInsTest2() {
+  const x = new Variable(1);
+
+  //[v2 x l1>< l2 l3]
+  const s = new Tag(Orientation.EW,
+    [
+      new Variable(2),
+      x,
+      new Literal(1)
+    ],
+    [
+      new Literal(2),
+      new Literal(3)
+    ]);
+
+  //[v3><s]
+  const p = new Tag(Orientation.EW, [new Variable(3)], [s]);
+
+  const action = new AssociativeExtract(x, Quadrant.SE);
+  action.apply();
+
+  const expectedSibling = new Tag(Orientation.EW,
+    [
+      new Variable(2),
+      new Literal(1)
+    ],
+    [
+      new Literal(2),
+      new Literal(3)
+    ]);
+
+  const expectedParent = new Tag(Orientation.EW, [new Variable(3)], [x, s]);
+
+  return assertAA(p, expectedParent, "AssociativeInsert test 2 failed");
+}
+
+function DTest1() {
+
+  const inner = new Tag(Orientation.EW);
+  i1 = new Variable(1);
+  i2 = new Variable(2);
+  inner.addNorthWest(i1);
+  inner.addNorthWest(i2);
+
+  const factor = new Literal(1);
+  const before = new Tag(Orientation.NS, [factor, inner]);
+
+  const expected = new Tag(Orientation.EW,
+   [
+     new Tag( Orientation.NS, [factor, i1]),
+     new Tag( Orientation.NS, [factor, i2])
+   ]);
+
+   //Apply
+
+   return assertAA(before, expected, "Distribute test 1 failed");
+}
+
+function DTest2() {
+
+  v1 = new Variable(1);
+  v2 = new Variable(2);
+  v3 = new Variable(3);
+  l1 = new Literal(1);
+  l2 = new Literal(2);
+  l3 = new Literal(3);
+  //[v3, l1 >< l2]
+  const x = new Tag(Orientation.NS, [v3, l1], [l2])
+  //[v1, v2, x, l3 >< ]
+  const inner = new Tag(Orientation.EW, [v1, v2, x, l3]);
+
+  factor = new Variable(4);
+  factor = negative(factor);
+  const before = new Tag(Orientation.NS, [factor, inner]);
+
+  const expected = new Tag(Orientation.EW, [
+    new Tag(Orientation.NS, [factor, v1]),
+    new Tag(Orientation.NS, [factor, v2]),
+    new Tag(Orientation.NS, [factor, x]),
+    new Tag(Orientation.NS, [factor, l3])
+  ]);
+
+  //apply Distribute
+
+  return assertAA(before, expected, "Distribute test 2 failed");
+}
+
+function FTest1() {
+
+  const factor = new Literal(1);
+  i1 = new Variable(1);
+  i2 = new Variable(2);
+  i3 = new Literal(5);
+
+  const before = new Tag(Orientation.EW,
+    [
+      new Tag(Orientation.NS, [factor, i1]),
+      new Tag(Orientation.NS, [factor, i2]),
+      i3
+    ]);
+
+  const inner = new Tag(Orientation.EW);
+  inner.addNorthWest(i1);
+  inner.addNorthWest(i2);
+
+  const factored = new Tag(Orientation.NS, [factor, inner]);
+  const expected = new Tag(Orientation.EW, [factored, i3]);
+
+   //Apply Factor
+
+   return assertAA(before, expected, "Factor test 1 failed");
+}
+
+function FTest2() {
+
+  v1 = new Variable(1);
+  v2 = new Variable(2);
+  v3 = new Variable(3);
+  l1 = new Literal(1);
+  l2 = new Literal(2);
+  l3 = new Literal(3);
+  factor = new Variable(4);
+  factor = negative(factor);
+
+  const before = new Tag(Orientation.EW, [
+    new Tag(Orientation.NS, [factor, v1]),
+    new Tag(Orientation.NS, [factor, v2]),
+    new Tag(Orientation.NS, [factor, x]),
+    new Tag(Orientation.NS, [factor, l3])
+  ]);
+
+  //[v3, l1 >< l2]
+  const x = new Tag(Orientation.NS, [v3, l1], [l2])
+
+  //[v1, v2, x, l3 >< ]
+  const inner = new Tag(Orientation.EW, [v1, v2, x, l3]);
+
+  const expected = new Tag(Orientation.NS, [factor, inner]);
+
+  //apply Factor
+
+  return assertAA(before, expected, "Factor test 2 failed");
+}
+
+function SFTest() {
+
+
+}
+
 function testAll() {
 
   try {
@@ -311,14 +495,23 @@ function testAll() {
     if(AMTest1()&&AMTest2()&&AMTest3()) {
       console.log("All AssociativeMerge tests have passed");
     }
-    if(AETest1()&&AETest2()) {
-      console.log("All AssociativeExtract tests have passed");
-    }
     if(AITest1()&&AITest2()) {
       console.log("All AssociativeIntro tests have passed");
     }
+    if(AInsTest1()&&AInsTest2()) {
+      console.log("All AssociativeInsert tests have passed");
+    }
+    if(AETest1()&&AETest2()) {
+      console.log("All AssociativeExtract tests have passed");
+    }
+    if(DTest1()&&DTest2()) {
+      console.log("All Distribute tests have passed");
+    }
+    if(FTest1()&&FTest2()) {
+      console.log("All Factor tests have passed");
+    }
   } catch(error) {
-    console.log("Errors have occured")
+    console.log("Algebraic Action Reference errors has occurred");
   }
 }
 
