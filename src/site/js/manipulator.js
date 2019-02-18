e7e7eb16cde26eea00e87802ba2fc98d14d66dc2
fractalyst <fractalyst97@gmail.com>
2019-02-17 20:16:57 -0500

    manipulator page can now display new problems without reloading page

--- src/site/js/manipulator.js
+++ src/site/js/manipulator.js
@@ -2,25 +2,67 @@
 var problem_to_load=getProblemFromURL();
 
 window.onload = () => {
-    displayProblem(problem_to_load);
+    displayProblemFromDB(problem_to_load);
 };
-
-function displayProblem(problem){
-    var canvas = document.getElementById("canvas-container");
-    while (canvas.lastChild) {
-      canvas.removeChild(canvas.lastChild);
+window.onpopstate = (e) =>{
+    if(e.state){
+        problem_to_load=e.state;
+        displayProblemFromDB(e.state);
     }
-    if(problem.toString().indexOf('_PROBLEM')===1){
-        decompress_string_js(problem.expression_start,decomp => {
-            $("#canvas-container").append(Deserialize(decomp).render());
-        });
+};
+//put undefined in place of argument you want to skip and it will be default
+function displayProblemFromStruct(problem, container_id_working="canvas-container", container_id_goal="goal-container"){
+    if(problem_to_load!==null){
+        problem_to_load=null;
     }else{
-        get_problem_from_db(problem, res => {
-            decompress_string_js(res.expression_start,decomp => {
-                $("#canvas-container").append(Deserialize(decomp).render());
-            });
+        history.pushState(problem_id, undefined, '/manipulator/problems/'+problem_id);
+    }
+    if(container_id_working!==null){
+        decompress_string_js(problem.expression_start,decomp => {
+            let working_canvas = document.getElementById(container_id_working);
+            while (working_canvas.lastChild) {
+                working_canvas.removeChild(working_canvas.lastChild);
+            }
+            $(working_canvas).append(Deserialize(decomp).render());
         });
     }
+    if(container_id_goal!==null){
+        decompress_string_js(problem.expression_start,decomp => {
+            let goal_canvas = document.getElementById(container_id_goal);
+            while (goal_canvas.lastChild) {
+                goal_canvas.removeChild(goal_canvas.lastChild);
+            }
+            $(goal_canvas).append(Deserialize(decomp).render());
+        });
+    }
+}
+//put undefined in place of argument you want to skip and it will be default
+function displayProblemFromDB(problem_id, container_id_working="canvas-container", container_id_goal="goal-container"){
+    if(problem_to_load!==null){
+        problem_to_load=null;
+    }else{
+        history.pushState(problem_id, undefined, '/manipulator/problems/'+problem_id);
+    }
+    get_problem_from_db(problem_id, res => {
+        decompress_string_js(res.expression_start,decomp => {
+            if(container_id_working!==null){
+                let working_canvas = document.getElementById(container_id_working);
+                while (working_canvas.lastChild) {
+                    working_canvas.removeChild(working_canvas.lastChild);
+                }
+                $(working_canvas).append(Deserialize(decomp).render());
+            }
+        });
+        decompress_string_js(res.expression_goal,decomp => {
+            if(container_id_working!==null){
+                let goal_canvas = document.getElementById(container_id_goal);
+                while (goal_canvas.lastChild) {
+                    goal_canvas.removeChild(goal_canvas.lastChild);
+                }
+                $(goal_canvas).append(Deserialize(decomp).render());
+            }
+        });
+    });
 }
 
 function getProblemFromURL(){
