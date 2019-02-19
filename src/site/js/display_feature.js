7261093cfb35973256027f2997f4c7518d4f7fab
Fractalyst <fractalyst97@gmail.com>
2019-02-19 10:34:32 -0500

    Explorer Page working

---
+++ src/site/js/display_feature.js
@@ -0,0 +1,44 @@
+//put undefined in place of argument you want to skip and it will be default
+function displayProblemFromStruct(problem, container_id_working="canvas-container", container_id_goal="goal-container"){
+    if(container_id_working!==null){
+        decompress_string_js(problem.expression_start,decomp => {
+            let working_canvas = document.getElementById(container_id_working);
+            while (working_canvas.lastChild) {
+                working_canvas.removeChild(working_canvas.lastChild);
+            }
+            $(working_canvas).append(Deserialize(decomp).render());
+        });
+    }
+    if(container_id_goal!==null){
+        decompress_string_js(problem.expression_start,decomp => {
+            let goal_canvas = document.getElementById(container_id_goal);
+            while (goal_canvas.lastChild) {
+                goal_canvas.removeChild(goal_canvas.lastChild);
+            }
+            $(goal_canvas).append(Deserialize(decomp).render());
+        });
+    }
+}
+//put undefined in place of argument you want to skip and it will be default
+function displayProblemFromDB(problem_id, container_id_working="canvas-container", container_id_goal="goal-container"){
+   get_problem_from_db(problem_id, res => {
+        decompress_string_js(res.expression_start,decomp => {
+            if(container_id_working!==null){
+                let working_canvas = document.getElementById(container_id_working);
+                while (working_canvas.lastChild) {
+                    working_canvas.removeChild(working_canvas.lastChild);
+                }
+                $(working_canvas).append(Deserialize(decomp).render());
+            }
+        });
+        decompress_string_js(res.expression_goal,decomp => {
+            if(container_id_goal!==null){
+                let goal_canvas = document.getElementById(container_id_goal);
+                while (goal_canvas.lastChild) {
+                    goal_canvas.removeChild(goal_canvas.lastChild);
+                }
+                $(goal_canvas).append(Deserialize(decomp).render());
+            }
+        });
+    });
+}
--- src/site/js/manipulator.js
+++ src/site/js/manipulator.js
@@ -9,64 +9,15 @@ window.onpopstate = (e) =>{
         displayProblemFromDB(e.state);
     }
 };
-//put undefined in place of argument you want to skip and it will be default
-function displayProblemFromStruct(problem, container_id_working="canvas-container", container_id_goal="goal-container"){
-    if(problem_to_load!==null){
-        problem_to_load=null;
-    }else{
-        history.pushState(problem_id, undefined, '/manipulator/problems/'+problem_id);
-    }
-    if(container_id_working!==null){
-        decompress_string_js(problem.expression_start,decomp => {
-            let working_canvas = document.getElementById(container_id_working);
-            while (working_canvas.lastChild) {
-                working_canvas.removeChild(working_canvas.lastChild);
-            }
-            $(working_canvas).append(Deserialize(decomp).render());
-        });
-    }
-    if(container_id_goal!==null){
-        decompress_string_js(problem.expression_start,decomp => {
-            let goal_canvas = document.getElementById(container_id_goal);
-            while (goal_canvas.lastChild) {
-                goal_canvas.removeChild(goal_canvas.lastChild);
-            }
-            $(goal_canvas).append(Deserialize(decomp).render());
-        });
-    }
-}
-//put undefined in place of argument you want to skip and it will be default
-function displayProblemFromDB(problem_id, container_id_working="canvas-container", container_id_goal="goal-container"){
-    if(problem_to_load!==null){
-        problem_to_load=null;
-    }else{
-        history.pushState(problem_id, undefined, '/manipulator/problems/'+problem_id);
-    }
-    get_problem_from_db(problem_id, res => {
-        decompress_string_js(res.expression_start,decomp => {
-            if(container_id_working!==null){
-                let working_canvas = document.getElementById(container_id_working);
-                while (working_canvas.lastChild) {
-                    working_canvas.removeChild(working_canvas.lastChild);
-                }
-                $(working_canvas).append(Deserialize(decomp).render());
-            }
-        });
-        decompress_string_js(res.expression_goal,decomp => {
-            if(container_id_goal!==null){
-                let goal_canvas = document.getElementById(container_id_goal);
-                while (goal_canvas.lastChild) {
-                    goal_canvas.removeChild(goal_canvas.lastChild);
-                }
-                $(goal_canvas).append(Deserialize(decomp).render());
-            }
-        });
-    });
-}
+
 
 function getProblemFromURL(){
     let prob=(window.location.href).substr((window.location.href).lastIndexOf("/")+1).replace('.html','').replace('.json','');
     if(prob==='manipulator'){
+        return null;
+    }
+    if(prob===('null')){
+        alert("Error(manipulator.js): Please enter a problem in the URL or select a problem from another page");
         return null;
     }
     return prob;
