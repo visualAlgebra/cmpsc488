ef091f38c52af87f88bd7c2d309b38ac2d9579a1
Fractalyst <fractalyst97@gmail.com>
2019-02-19 14:44:22 -0500

    user profile page working

--- src/site/js/db_management.js
+++ src/site/js/db_management.js
@@ -5,13 +5,13 @@ function get_problem_from_db(problem_id, callback){
           callback(new ProblemInfo(problem_id,JSON.parse(http.responseText).startExpression,JSON.parse(http.responseText).goalExpression));
         }
     }
-  http.open("GET", "http://localhost:8080/problems/"+problem_id);
+  http.open("GET", "http://localhost:8080/problems/"+problem_id, true);
   http.send();
 }
 function post_problem_from_site(problem){
   try{
     let http=new XMLHttpRequest();
-    http.open("POST", "http://localhost:8080/problems/"+problem.problem_id);
+    http.open("POST", "http://localhost:8080/problems/"+problem.problem_id, true);
     http.setRequestHeader("Content-type", "application/json");
     user="Fractalyst, ofcourse, why would any other user be here?";
     http.setRequestHeader("Account", user);
@@ -20,7 +20,7 @@ function post_problem_from_site(problem){
     str+=      ' "goalExpression": ['+problem.expression_goal+'],';
     str+=      ' "createdBy": "'+user+'" }';
     console.log(str);
-  http.send(str);
+    http.send(str);
   }
   catch(e){}
 }
@@ -31,5 +31,19 @@ function delete_problem_from_db(problem_id, acc_id){
   http.open("DELETE", "http://localhost:8080/problems/"+problem_id, true);
   http.setRequestHeader("Content-type", "application/json");
   http.setRequestHeader('Account', acc_id);
+  http.send();
+}
+
+function get_account_from_db(account_id, callback){
+  let http=new XMLHttpRequest();
+  http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          callback(new AccountInfo(account_id,
+            JSON.parse(http.responseText).timeCreated,
+            JSON.parse(http.responseText).creations));
+        }
+    }
+  http.open("GET", "http://localhost:8080/accounts/"+account_id, true);
+  http.setRequestHeader('account', account_id);
   http.send();
 }
