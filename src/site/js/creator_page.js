10449c1519aaca6812c631320530a48dc86f2b24
Fractalyst <fractalyst97@gmail.com>
2019-03-19 17:59:30 -0400

    Start Vue stuff

---
+++ src/site/js/creator_page.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import ExpressionTree from "./vue_components/ExpressionTree";
+import {Variable} from "./expression_tree";
+import ToolBar from "./vue_components/ToolBar";
+
+export const creator_page = new Vue({
+  name: "Root",
+
+  el: "#vueCanvasContainer",
+
+  template: `
+  <div xmlns="http://www.w3.org/1999/xhtml">
+    <ToolBar></ToolBar>
+    <ExpressionTree v-if="workingExpressionTree":tree="workingExpressionTree">
+    
+</ExpressionTree>
+  </div>
+  `,
+
+  data() {
+    return {
+      workingExpressionTree: new Variable(6),
+    };
+  },
+
+  components: {
+    ExpressionTree,
+    ToolBar,
+  },
+});
---
+++ src/site/js/vue_components/ToolBar.js
@@ -0,0 +1,7 @@
+export default {
+  name: "ToolBar",
+
+  template: `
+  <p>Tool Bar</p>
+  `,
+};
