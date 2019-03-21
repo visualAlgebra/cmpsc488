a9ac7f0bef71d8957ab20bebee5766b9437c1872
eignnx <eignnx@gmail.com>
2019-03-20 20:27:05 -0400

    Apply `hoverable` class on Vue-rendered expr trees.

--- src/site/js/vue_components/ExprTreeTagButton.js
+++ src/site/js/vue_components/ExprTreeTagButton.js
@@ -1,12 +1,23 @@
 export default {
   name: "ExprTreeTagButton",
 
+  props: ["hoverable"],
+
   template: `
   <div
     xmlns="http://www.w3.org/1999/xhtml"
     class="tag-button-column"
    >
-    <div xmlns="http://www.w3.org/1999/xhtml" class="tag-button"></div>
+    <div xmlns="http://www.w3.org/1999/xhtml" :class="classes"></div>
   </div>
   `,
+
+  computed: {
+    classes() {
+      return [
+          "tag-button",
+          this.hoverable ? "hoverable" : "",
+      ]
+    },
+  },
 };
--- src/site/js/vue_components/ExprTreeVariable.js
+++ src/site/js/vue_components/ExprTreeVariable.js
@@ -1,11 +1,20 @@
 export default {
   name: "ExprTreeVariable",
 
-  props: ["tree"],
+  props: ["tree", "hoverable"],
 
   template: `
-  <div xmlns="http://www.w3.org/1999/xhtml" class="variable">
+  <div xmlns="http://www.w3.org/1999/xhtml" :class="classes">
     {{ "x" + tree.value }}
   </div>
   `,
+
+  computed: {
+    classes() {
+      return [
+        "variable",
+        this.hoverable ? "hoverable" : "",
+      ]
+    },
+  },
 };
