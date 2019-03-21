45890a49ad4640fa076ef0232d366a97c3367243
mitzeljw <mitzeljw@gmail.com>
2019-03-20 20:28:31 -0400

    bug fixes with deletes in firestore_database & test cases

--- src/server/request_test.js
+++ src/server/request_test.js
@@ -211,7 +211,6 @@ function deleteFileTest(fileName, testNumber, accountID, successStatusCode) {
 
 
 
-
 function executeFirebaseTesting() {
   postFileTest("accounts/TEST_USER_0", 0, JSON.stringify({"bio":"a bio for this account","accountID":"TEST_USER_0"}), "TEST_USER_0", 400);
   postFileTest("problems/TEST_USER_0/TEST_PROBLEM_0", 1, JSON.stringify({ "problemName": "TEST_PROBLEM_0",
@@ -219,6 +218,33 @@ function executeFirebaseTesting() {
   "goalExpression":[93,0,0,0,2,125,0,0,0,0,0,0,0,0,61,-99,5,-48,-24,113,90,8,-110,32,113,-87,-75,-61,4,117,-40,123,0,72,-105,88,-31,114,116,95,54,114,-67,59,-65,-107,-49,27,74,-17,-61,80,-60,56,48,124,95,-92,-13,-10,-86,-126,96,29,-88,-51,-1,124,84,78,108,-30,-47,-61,115,-124,28,127,-1,88,111,-128,0],
   }), "TEST_USER_0",400);
   postFileTest("lessons/TEST_USER_0/TEST_LESSON_0", 2, JSON.stringify({"creations": ["problems/TEST_USER_0\\TEST_PROBLEM_0"], "creatorAccountID": "TEST_USER_0"}), "TEST_USER_0", 400);
+
+  setTimeout(function () {
+    postFileTest("accounts/TEST_USER_1", 3, JSON.stringify({"bio":"a bio for this account","accountID":"TEST_USER_1"}), "TEST_USER_1", 201);
+
+    setTimeout(function () {
+      postFileTest("problems/TEST_USER_1/TEST_PROBLEM_1", 4, JSON.stringify({ "problemName": "TEST_PROBLEM_1",
+      "startExpression": [93,0,0,0,2,-28,0,0,0,0,0,0,0,0,61,-99,4,-88,114,97,90,8,-85,11,71,75,-96,89,-84,-114,-34,11,-70,-102,-115,-125,78,39,99,64,-75,27,-123,-124,9,-43,-34,-39,34,-85,-19,125,39,-109,103,58,54,86,-52,-56,10,32,-121,-3,-38,-16,104,-56,79,-36,88,1,-72,66,-41,17,-1,-119,-42,-1,-19,88,-72,0],
+      "goalExpression":[93,0,0,0,2,125,0,0,0,0,0,0,0,0,61,-99,5,-48,-24,113,90,8,-110,32,113,-87,-75,-61,4,117,-40,123,0,72,-105,88,-31,114,116,95,54,114,-67,59,-65,-107,-49,27,74,-17,-61,80,-60,56,48,124,95,-92,-13,-10,-86,-126,96,29,-88,-51,-1,124,84,78,108,-30,-47,-61,115,-124,28,127,-1,88,111,-128,0],
+      }), "TEST_USER_1",201);
+
+      setTimeout(function() {
+        postFileTest("lessons/TEST_USER_1/TEST_LESSON_1", 5, JSON.stringify({"creations": ["problems/TEST_USER_1\\TEST_PROBLEM_1"], "creatorAccountID": "TEST_USER_1"}), "TEST_USER_1", 201);
+
+        setTimeout(function() {
+          deleteFileTest("problems/TEST_USER_1/TEST_PROBLEM_1", 6, "TEST_USER_1", 200);
+
+          setTimeout(function() {
+            deleteFileTest("lessons/TEST_USER_1/TEST_LESSON_1", 7, "TEST_USER_1", 200);
+
+            setTimeout(function() {
+              deleteFileTest("accounts/TEST_USER_1", 8, "TEST_USER_1", 200);
+            }, 3000);
+          }, 3000);
+        }, 3000);
+      }, 3000);
+    }, 3000);
+  }, 3000);
   // postFileTest("")
   // getFileTest("accounts/TEST_USER_0",0,"");
 	// getFileTest("lessons/TEST_LESSON_0",1,"");
