253ebe9549b1c62453f46a2619f847a5d6fcaeb2
fractalyst <fractalyst97@gmail.com>
2019-03-21 20:49:58 -0400

    Revert just bare necessities to keep an actual working tree on page

--- src/site/js/manipulator.js
+++ src/site/js/manipulator.js
@@ -1,19 +1,29 @@
-import {globals} from './gui';
-import {displayTreeFromDBStruct} from './display_feature';
-import {addHistoryEntry, clearHist, histAction, renderHist, setGoalTree} from "./history_nav";
+import {globals, mouse} from './gui';
+import {displayProblemFromDB, displayTreeFromDBStruct, displayProblemFromDBStruct} from './display_feature';
+import {addHistoryEntry, histAction, clearHist, renderHist, setGoalTree} from "./history_nav";
 import {Deserialize} from "./expression_tree";
 import {manipulatorWindow} from "./manipulator_window";
-import {manipulator_Vue} from "./manipulator_vue";
+import {createDummyProblem} from "./random_expression_creator";
+import {manipulatorVue} from "./manipulator_vue";
 
-export function historyAction(bool){
-  let t=histAction(bool);
-  if(t!==null){
-    globals.workingExpressionTree=Deserialize(t);
-    displayTreeFromDBStruct(t, 'canvasContainer');
+var problem_to_load=getProblemFromURL();
+window.onload=()=>{
+  changeMouseMode(0);
+  if(problem_to_load!==null){
+    displayProblemFromDB(problem_to_load, 'canvasContainer', null, (res, res2)=>{
+      onDisplay(res, res2);
+    });
   }
-}
+};
+window.onpopstate=(e)=>{
+  if(e.state){
+    problem_to_load=e.state;
+    displayProblemFromDB(e.state);
+  }
+};
 
-export function restart(){
+
+function restart(){
   displayTreeFromDBStruct(document.getElementById("restartButton").dataset.str, 'canvasContainer', res=>{
     alert("Restarted");
     clearHist();
@@ -21,23 +31,52 @@ export function restart(){
   });
 }
 
-export function drawCanvas(){
+function drawCanvas(){
   renderHist("histCanvas", document.getElementById("canvasContainer").dataset.str);
   renderHist("histCanvas", document.getElementById("canvasContainer").dataset.str);
 }
 
-function touchHistCanvas(x, y){
+function touchHistCanvas(x,y){
   console.log(x+", "+y);
 }
 
-export function onDisplay(res, containerId){
+function insertMenu(type){
+  if(type){
+    document.getElementById("manipulatorsSubPanel").style.display="inline";
+  }else{
+    document.getElementById("manipulatorsSubPanel").style.display="none";
+  }
+}
+
+function onDisplay(res, containerId){
   if(containerId==="canvasContainer"){
     globals.workingExpressionTree=res;
-    manipulatorWindow.workingExpressionTree=res;
+    manipulatorWindow.workingExpressionTree = res;
     addHistoryEntry(res);
-    document.getElementById("restartButton").setAttribute("data-str", res.toString());
   }else if(containerId==="goalContainer"){
     let temp=document.getElementById("goalContainer").dataset.str;
     setGoalTree(temp);
   }
 }
+
+function changeMouseMode(num){
+  insertMenu(false);
+  if(num===0){
+    mouse.mode="General Manipulation";
+  }else if(num===1){
+    mouse.mode="Merging Literals";
+  }else if(num===2){
+    mouse.mode="Distribution";
+  }
+}
+
+function getProblemFromURL(){
+  let prob=(window.location.href).substr((window.location.href).indexOf('/manipulator/problems/'));
+  if(prob.indexOf('manipulator/problems/')=== -1||prob==='null'||prob===''||prob==='undefined'){
+    //window.location.replace("../manipulator/problems/Getting%20Started");
+    alert("Error(manipulator.js): Please enter a problem after \"manipulator/problems/\" in the URL or select a problem from another page");
+    return null;
+  }
+  return prob.substr(22);
+}
+
