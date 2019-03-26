f16c30263d0de10126f01f2cbce4bfcea84c82a4
eignnx <eignnx@gmail.com>
2019-03-26 16:40:15 -0400

    Add pan-zoom support for expression tree on Manipulator page

--- src/site/js/vue_components/ManipulatorWindow.js
+++ src/site/js/vue_components/ManipulatorWindow.js
@@ -1,4 +1,5 @@
 import ExpressionTree from "./ExpressionTree";
+import SvgPanZoom from "vue-svg-pan-zoom";
 
 export default {
   name: "ManipulatorWindow",
@@ -6,16 +7,56 @@ export default {
   props: ["tree"],
 
   template: `
-  <div xmlns="http://www.w3.org/1999/xhtml">
-    <ExpressionTree
-      v-if="tree"
-      :tree="tree"
-      interactive
-    ></ExpressionTree>
-  </div>
+<div
+  @dblclick="center()"
+>
+  <SvgPanZoom
+    :maxZoom="3"
+    :minZoom="0.5"
+    :zoomScaleSensitivity="0.1"
+    :dblClickZoomEnabled="false"
+    @svgpanzoom="registerSvgPanZoom"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="500px"
+    >
+      <foreignObject width="2000" height="1000">
+        <ExpressionTree
+          v-if="tree"
+          :tree="tree"
+          interactive
+        ></ExpressionTree>
+      </foreignObject>
+    </svg>
+  </SvgPanZoom>
+</div>
   `,
+
+  data: () => ({
+    svgpanzoom: null,
+  }),
+
+  mounted() {
+    this.center();
+  },
+
+  methods: {
+    registerSvgPanZoom(svgpanzoom) {
+      this.svgpanzoom = svgpanzoom;
+    },
+
+    center() {
+      this.svgpanzoom.center();
+      this.svgpanzoom.resetZoom();
+      this.svgpanzoom.pan({x:300, y:200});
+      console.log(this.svgpanzoom);
+    },
+  },
 
   components: {
     ExpressionTree,
+    SvgPanZoom,
   },
 }
