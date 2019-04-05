d2fb759d3e28b247acea19a459cd8ede35525156
eignnx <eignnx@gmail.com>
2019-04-05 11:25:28 -0400

    Pass mouse instance `this` vue component

--- src/site/js/creator_vue.js
+++ src/site/js/creator_vue.js
@@ -36,7 +36,7 @@ export const creator_vue=new Vue({
     desc:"",
     time:"",
     goalTreeStr: null,
-    mouse: new Mouse(),
+    mouse: new Mouse(this),
     lessonID: null,
   }),
   mounted() {
