c977d80b8dc101b81ce515aa761d9e425043388d
eignnx <eignnx@gmail.com>
2019-04-12 10:36:08 -0400

    Add text to win modal

--- src/site/js/vue_components/WinModal.js
+++ src/site/js/vue_components/WinModal.js
@@ -1,8 +1,21 @@
 export default {
   name: "WinModal",template: `
   <div id="lessonModal" class="modal">
-      <div class="modal-content">
-        <h4 class="black-text">You've Won!</h4>
+      <div class="modal-content center-align">
+        <div class="row">
+          <h1 class="black-text">Success!</h1>
+          <p class="black-text">
+            You were able to transform the starting tree into the goal tree. Nice job!
+          </p>
+        </div>
+        <div class="row">
+          <a
+            class="waves-effect waves-green btn-flat"
+            href="http://localhost:8080/explorer.html"
+          >
+            Return to Explore Page
+          </a>
+        </div>
       </div>
   </div>  
   `
