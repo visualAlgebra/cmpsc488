76371b65eb75d9eb1f61174df57a669c23c16abc
eignnx <eignnx@gmail.com>
2019-04-12 16:01:58 -0400

    Truncate text on nav buttons

--- src/site/js/vue_components/CreatorNavigationButtons.js
+++ src/site/js/vue_components/CreatorNavigationButtons.js
@@ -16,42 +16,42 @@ export default {
     <div class="row spaced-out-row">
       <a v-bind:href="url" class="tab waves-effect waves-light btn col" v-on:click="load()">
           <i class="material-icons left">folder_open</i>
-          Load
+          <span class="truncate">Load</span>
       </a>
       <a class="tab waves-effect waves-light btn col" v-on:click="clear()">
           <i class="material-icons left">clear</i>
-          Clear
+          <span class="truncate">Clear</span>
       </a>
       <a class="tab waves-effect waves-light btn col" v-on:click="undo()">
           <i class="material-icons left">undo</i>
-          Undo
+          <span class="truncate">Undo</span>
       </a>
       <a class="tab waves-effect waves-light btn col" v-on:click="redo()">
           <i class="material-icons left">redo</i>
-          Redo
+          <span class="truncate">Redo</span>
       </a>
       <a class="tab waves-effect waves-light btn col modal-trigger" data-target="algebraicModal">
           <i class="material-icons left">info</i>
-          Reference
+          <span class="truncate">Reference</span>
       </a>
       <a class="tab waves-effect waves-light btn col sidenav-trigger" data-target="histNav">
           <i class="material-icons left">history</i>
-          History
+          <span class="truncate">History</span>
       </a>
       <a class="tab waves-effect waves-light btn col" v-on:click="generate()">
           <i class="material-icons left">lightbulb_outline</i>
-          Generate
+          <span class="truncate">Generate</span>
       </a>
       <a class="tab waves-effect waves-light btn col" v-on:click="clearStartStage()" v-if="stage==='build'">
           <i class="material-icons left">navigate_next</i>
-          Create Goal
+          <span class="truncate">Create Goal</span>
       </a>
       <a class="tab waves-effect waves-light btn col" v-on:click="clearGoalStage()" v-if="stage==='manip'">
           <i class="material-icons left">navigate_before</i>
-          Change Start
+          <span class="truncate">Change Start</span>
       </a>
-      <AlgebraicActionsModalPopup></AlgebraicActionsModalPopup>
     </div>
+    <AlgebraicActionsModalPopup></AlgebraicActionsModalPopup>
   </div>  
   `,
   data: () => ({
