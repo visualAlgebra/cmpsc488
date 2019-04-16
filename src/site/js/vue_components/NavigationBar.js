375e7dea0934cd063852a916fe2977e89a981e1b
Fractalyst <fractalyst97@gmail.com>
2019-04-16 12:30:10 -0400

    bugfix navbar

--- src/site/js/vue_components/NavigationBar.js
+++ src/site/js/vue_components/NavigationBar.js
@@ -50,7 +50,7 @@ export default {
         <a href="http://localhost:8080/creator.html">Create</a>
       </li>
       <li>
-        <a href="http://localhost:8080/manipulator.html">Manipulate</a>
+        <a href="http://localhost:8080/manipulator/problems/Getting_started">Manipulate</a>
       </li>
       <li>
         <a href="http://localhost:8080/explorer.html">Explore</a>
