1795bf48948c09af2faf0191e9e93e508bbfe3f0
eignnx <eignnx@gmail.com>
2019-04-23 11:03:16 -0400

    Reformat "index page" Vue components

--- src/site/js/index_vue.js
+++ src/site/js/index_vue.js
@@ -4,32 +4,46 @@ import IndexPageViewVue from "./vue_components/IndexPageViewVue";
 import {addListenerForUser} from "./user_system";
 
 export const index_vue = new Vue({
-  name: "Root", el: "#vue-app", template: `
-  <div>
-    <NavigationBar v-bind:user="userStruct" v-bind:oauth_user_getter="oauth_user_getter" v-bind:oauth_user_remover="oauth_user_remover" v-bind:logged="logged"></NavigationBar>
-    <IndexPageViewVue></IndexPageViewVue>
-  </div>
+  name: "Root",
+  el: "#vue-app",
+
+  template: `
+<div>
+  <NavigationBar
+    :user="userStruct"
+    :oauth_user_getter="oauth_user_getter"
+    :oauth_user_remover="oauth_user_remover"
+    :logged="logged"
+  ></NavigationBar>
+  <IndexPageViewVue/>
+</div>
   `,
-  data(){
-    return{
-      userStruct:null, logged:false,
+
+  data() {
+    return {
+      userStruct: null, logged: false,
     };
-  }, created(){
+  },
+
+  created() {
     addListenerForUser(this.oauth_user_getter);
   },
+
   methods: {
     oauth_user_getter(user) {
       this.userStruct = user;
       this.logged = true;
-    }, oauth_user_remover(){
-      this.usersStruct=null;
-      this.logged=false;
+    },
+
+    oauth_user_remover() {
+      this.usersStruct = null;
+      this.logged = false;
     },
   },
-  components:
-    {
-      NavigationBar, IndexPageViewVue,
-    }
-  ,
+
+  components: {
+    NavigationBar,
+    IndexPageViewVue,
+  },
 });
 
