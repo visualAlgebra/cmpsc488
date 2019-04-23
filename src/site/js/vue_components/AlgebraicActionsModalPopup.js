f275104b31053beb21c3ba458595153911a6458c
Toan <nguyen.toan.0397@gmail.com>
2019-04-22 20:52:19 -0400

    Added gifs for Reference Modal

--- src/site/js/vue_components/AlgebraicActionsModalPopup.js
+++ src/site/js/vue_components/AlgebraicActionsModalPopup.js
@@ -9,7 +9,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Commutative Swap</div>
             <div class="collapsible-body black-text">
-              <img src="http://localhost:8080/src/site/assets/com_swap_160.gif"><br>
+              <img src="http://localhost:8080/src/site/assets/CommutativeSwap.gif"><br>
               <span>You may swap the positions of two elements(tag, unknown, or atom) in the same tag quadrant. This action will not work on elements 
                 of opposite quadrants, even if they are in the same outer parent.
               </span><br>
@@ -19,6 +19,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Associative Merge</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AssociativeMerge.gif"><br>
               <span>If an inner tag has the same orientation as its outer tag, the outer tag can be collapsed, leaving only the inner tag. There must be
               two tags and the two tags must be the same orientation.</span><br>
               <p><b>To Initiate:</b> Select Transform mode. Drag the tag button</p>
@@ -27,6 +28,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Associative Introduction</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AssociativeIntro.gif"><br>
               <span>Elements of an expression tree may be enclose with another tag of the same orientation. This action works on any element.
               Though there is no limit to this action, try not to abuse.</span><br>
               <p><b>To initiate:</b> Select Transform mode. Click the element to enclose.</p>
@@ -35,6 +37,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Associative Extract</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AssociativeExtract.gif"><br>
               <span>If an inner tag has the saDrag one of the elements to factor into the East-West tag.me orientation as the outer tag, then the elements of 
                 the inner tag may be taken out into the outer tag.</span><br>
               <p><b>To initiate:</b> Select Tranform mode. Drag the element to extract in the inner tag into the outer tag</p>
@@ -43,6 +46,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Associative Insert</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AssociativeInsert.gif"><br>
               <span>If an inner tag has the same orientation as the outer tag, then the elements of the outer tag may be placed into the inner tag.</span><br>
               <p><b>To initiate:</b> Select Transform mode. Drag the element to insert from the outer tag into the inner tag.</p>
             </div>
@@ -50,6 +54,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Distribute</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/Distribute.gif"><br>
               <span>When a North-South tag contains an East-West tag and another element, that element may be distributed through the East-West tag.
               Drag the element to distribute onto the North-South Tag. The resulting tag will  If the North-South tag does not have any other elements besides those two, then 
               the North-South tag will be collapsed.</span><br>
@@ -59,6 +64,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Factor</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/Factor.gif"><br>
               <span>When an East-West tag with North-South tags contain the the same element, that element may be factored out of the East-West tags, 
               creating a North-South tag with the factored element and the remaining elements of the East-West tag. If one of the elements in the 
               is just the element to factor, that element will reduce to the identity atom. To initiate this action, the element
@@ -69,6 +75,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Split Fraction</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/SplitFrac.gif"><br>
               <span>Suppose we have a North-South tag. Let's call it a fraction. If there is a single a single East-West tag is in the north quadrant of the 
               fraction, call it the numerator, and some elements in the south quadrant of the North-South tag, call it the denominator, then the fraction may be 
               split up such that the resulting tag is an East-West tag containing North-South tags. For each element in the summation, the resulting tag will have 
@@ -79,6 +86,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Combine Fraction</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/CombineFrac.gif"><br>
               <span>Fractions in a East-West tag may be combined into one tag. The numerators of the fractions must have the same denominator. This action
                 will also work if used on fractions in the east and west quadrants. 
               </span><br>
@@ -88,6 +96,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Quadrant Flip</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/QuadrantFlip.gif"><br>
               <span>If an inner tag has the same orientation as the outer tag, then the inner tag may be flipped to the the opposite quadrant along with it's 
                 quadrants being swapped. 
               </span><br>
@@ -97,6 +106,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Canceling</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/Cancel.gif"><br>
               <span>If two elements in opposite quadrants are equal, then the two elements may be canceled. They must be the same. This will not work
                 if you attempt to drag a tag onto an equivalent tag, i.e. if you manipulate one of the tags you can cancel and then drag onto to the other tag,
                 this action will not be applied.               
@@ -107,12 +117,14 @@ export default {
           <li>
             <div class="collapsible-header black-text">Identity Balance</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/IdentityBalance.gif"><br>
               <span>Any element may be inserted into both quadrants of a tag.</span>
             </div>
           </li>
           <li>
             <div class="collapsible-header black-text">Atom Merge</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AtomMerge.gif"><br>
               <span>Two atoms may be combined.</span><br>
               <p><b>To Initiate:</b> Select Atom Merge mode. Drag one atom onto another.</p>
             </div>
@@ -120,6 +132,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Atom Absorption</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AtomAbsorption.gif"><br>
               <span>The zero atom in a North-South tag may destroy everything.</span><br>
               <p><b>To Initiate:</b> Select Atom Merge mode. Drag one atome onto another.</p>
             </div>
@@ -127,6 +140,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Atom Destruction</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AtomDestruction.gif"><br>
               <span>The identity may be removed from a tag</span>
               <p><b>To Initiate:</b> Select Atom Merge mode. Drag any element onto the identity</p>
             </div>
@@ -134,6 +148,7 @@ export default {
           <li>
             <div class="collapsible-header black-text">Atom Split</div>
             <div class="collapsible-body black-text">
+              <img src="http://localhost:8080/src/site/assets/AtomSplit.gif"><br>
               <span>Atoms may be converted into their equivalent atoms</span>
               <p><b>To Initiate:</b> Select Atom Merge mode. Click on any atom.</p>
             </div>
