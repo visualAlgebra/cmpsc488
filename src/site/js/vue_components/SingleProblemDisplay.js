c90797aac7f89a4b52a987d11e2c4d365f7ab7e6
Fractalyst <fractalyst97@gmail.com>
2019-04-24 21:24:50 -0400

    Explorer page now no longer allows apes to delete or edit problems
    by accident

--- src/site/js/vue_components/SingleProblemDisplay.js
+++ src/site/js/vue_components/SingleProblemDisplay.js
@@ -2,18 +2,18 @@ import ExpressionTree from "./ExpressionTree";
 import SingleExpressionDisplay from "./SingleExpressionDisplay";
 
 export default {
-  name: "SingleProblemDisplay", props: ["problemID", "start", "goal", "deleteProblem"], template: `
+  name: "SingleProblemDisplay", props: ["problemID", "start", "goal", "deleteProblem", "type"], template: `
   <div class="container" style="border-style: solid;border-color: black;">Problem: {{problemID}}
-    <a :href="edit" class="secondary-content">
+    <a v-if="type!=='explorer'" v-bind:href="edit" class="secondary-content">
       <i class="material-icons">rotate_left</i>
     </a>
-    <a :href="url" class="secondary-content">
+    <a v-bind:href="url" class="secondary-content">
       <i class="material-icons">send</i>
     </a>
-    <a class="secondary-content dropdown-trigger" :data-target="targetID">
+    <a v-if="type!=='explorer'" class="secondary-content dropdown-trigger" :data-target="targetID">
       <i class="material-icons">delete_forever</i>
     </a>
-    <ul :id="targetID" class='dropdown-content'>
+    <ul v-if="type!=='explorer'" v-bind:id="targetID" class='dropdown-content'>
       <li><a>Are you sure?</a></li>
       <li><a>No</a></li>
       <li><a v-on:click="deleteProb">Yes</a></li>
