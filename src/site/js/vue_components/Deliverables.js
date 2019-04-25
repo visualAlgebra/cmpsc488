aed787a5cae9997f97a6a2784fad6e1084b76ba8
eignnx <eignnx@gmail.com>
2019-04-24 21:34:02 -0400

    Put links in list items

--- src/site/js/vue_components/Deliverables.js
+++ src/site/js/vue_components/Deliverables.js
@@ -8,14 +8,32 @@ export default {
       <ul>
         <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Cmpsc488UseCaseEricKoskovich"> Use Case 1 </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/CreatingALessonUseCase.pdf"> Use Case 2 </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Domain Model.pdf"> Domain Model </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/High-level Requirements.pdf"> High-Level Requirements </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Project Glossary.pdf"> Project Glossary </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Storyboard.pdf"> Storyboard </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Use Case_ Create Shareable Link to Problem.pdf"> Use Case 3 </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Use Case_ Follow Link to a Problem.pdf"> Use Case 4 </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/Use Case_ User Requests Help.pdf"> Use Case 5 </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_1/UseCaseDiagram.pdf"> Use Case Diagram </a>
         </li>
       <ul>
@@ -25,16 +43,38 @@ export default {
       <ul>
         <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Class Diagram"> Class Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Create Shareable Link Robustness Diagram.pdf"> Create Shareable Link Robustness Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Create Shareable Link Sequence Diagram.pdf"> Create Shareable Link Sequence Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/CreateALessonRobustness.pdf"> Create a Lesson Robustness Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/CreateALessonSequenceDiagram.pdf"> Create a Lesson Sequence Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Follow Link to a Problem.pdf"> Follow Link to a Problem Robustness Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Follow Link to Problem (Sequence).pdf"> Follow Link to a Problem Sequence Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Generate Problem Robustness Final PDF.pdf"> Generate Problem Robustness Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Generate Problem Sequence Final PDF.pdf"> Generate Problem Sequence Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/List of Frameworks_Languages_etc.docx"> List of Frameworks and Languages </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Request Hint Robustness Diagram.png"> Request Hint Robustness Diagram </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_2/Request Hint Sequence Diagram.png"> Request Hint Sequence Diagram </a>
         </li>
       <ul>
@@ -44,6 +84,8 @@ export default {
       <ul>
         <li>
           <a download href="http://localhost:8080/documentation/Phase_3/Detailed Schedule.pdf"> Detailed Schedule </a>
+        </li>
+        <li>
           <a download href="http://localhost:8080/documentation/Phase_3/Gantt Chart - Sheet1.pdf"> Gantt Chart </a>
         </li>
       <ul>
