0b6eeb2f7794beae601cd73ca163a7544a3b0043
eignnx <eignnx@gmail.com>
2019-04-26 13:10:34 -0400

    Update homepage

--- src/site/js/vue_components/IndexPageViewVue.js
+++ src/site/js/vue_components/IndexPageViewVue.js
@@ -14,11 +14,34 @@ export default {
     </div>
     <h2 class="subtitle">A graphical, interactive, algebraic playground.</h2>
   </header>
+  
   <p class="flow-text">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis excepturi repellat repellendus! Ad, culpa dolorem eveniet illo laborum molestias quisquam voluptatum? A consequuntur earum esse placeat qui sunt tempora ullam!
+    Learn algebraic laws by solving challenges! Given a start expression, your job is to transform it into an equivalent goal expression.
   </p>
   
-  <section class="row literal-row">
+  <div style="display: flex; justify-content: center;  box-sizing: border-box;">
+    <div
+      style="border: solid 0.5rem rgba(255,255,255,0.1); border-radius: 0.5rem;"
+    >
+      <img
+        class="materialboxed"
+        width="600px"
+        src="http://localhost:8080/src/site/assets/Distribute.gif"
+      >
+    </div>
+  </div>
+    
+  <div class="row">
+    <h4 class="center-align">
+      <a href="http://localhost:8080/algebra/problems/Getting_started">Click here</a> to get started!
+    </h4>
+  </div>
+  
+  <p class="flow-text">
+     We simplify algebra by basing our graphical system on ℤ3, an algebraic field with <strong>only three numbers.</strong> Rather than getting bogged down in arithmetic, we want you to focus on the algebraic laws common to all kinds of algebras.
+  </p>
+  
+  <section class="row literal-row" style="margin-top: 5rem; margin-bottom: 5rem;">
     <div class="col s4">
       <div class="expr-tree-box hoverable">
         <ExpressionTree :tree="zero()" />
@@ -36,9 +59,6 @@ export default {
     </div>
   </section>
   
-  <p class="flow-text">
-     We simplify algebra by basing our graphical system on ℤ3, an algebraic field with <strong>only three numbers.</strong> Rather than getting bogged down in arithmetic, we want you to focus on the algebraic laws common to all kinds of algebras.
-  </p>
   <p class="flow-text">
     Solve puzzles on the <a href="http://localhost:8080/algebra/problems/Getting_started">Algebra page</a> by transforming one expression into another.
   </p>
