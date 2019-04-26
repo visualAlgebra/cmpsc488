1a47cf1c9d222a2a41de6b33be83bb56c5432fd2
eignnx <eignnx@gmail.com>
2019-04-26 15:32:11 -0400

    Clean up SingleProblemDisplay

--- src/site/js/vue_components/SingleProblemDisplay.js
+++ src/site/js/vue_components/SingleProblemDisplay.js
@@ -4,27 +4,42 @@ import SingleExpressionDisplay from "./SingleExpressionDisplay";
 export default {
   name: "SingleProblemDisplay", props: ["problemID", "start", "goal", "deleteProblem", "type"], template: `
   <div class="container single-problem-display">
-    <h5 class="center-align">Problem: {{problemID}}</h5>
-    <a v-if="type!=='explorer'" v-bind:href="edit" class="secondary-content">
-      <i class="material-icons">rotate_left</i>
-    </a>
-    <a v-bind:href="url" class="secondary-content">
-      <i class="material-icons">send</i>
-    </a>
-    <a v-if="type!=='explorer'" class="secondary-content dropdown-trigger" :data-target="targetID">
-      <i class="material-icons">delete_forever</i>
-    </a>
-    <ul v-if="type!=='explorer'" v-bind:id="targetID" class='dropdown-content'>
-      <li><a>Are you sure?</a></li>
-      <li><a>No</a></li>
-      <li><a v-on:click="deleteProb">Yes</a></li>
-    </ul>
+  
+    <h5 class="center-align" style="font-weight: bolder;">
+      <a :href="url">{{problemID}}</a>
+    </h5>
+    
     <div class="row">
-      <div class="col">Start:
+      <div class="col">
+        <h6>Start</h6>
         <SingleExpressionDisplay v-bind:tree="this.start"></SingleExpressionDisplay>
       </div>
-      <div class="col">Goal:
+      <div class="col">
+        <h6>Goal</h6>
         <SingleExpressionDisplay v-bind:tree="this.goal"></SingleExpressionDisplay>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col">
+        <a v-bind:href="url" class="secondary-content btn">
+          Play <i class="material-icons right">play_arrow</i>
+        </a>
+      </div>
+      <div class="col">
+        <a v-if="type!=='explorer'" v-bind:href="edit" class="secondary-content btn">
+          Edit <i class="material-icons right">edit</i>
+        </a>
+      </div>
+      <div class="col">
+        <a v-if="type!=='explorer'" class="secondary-content btn dropdown-trigger" :data-target="targetID">
+          Delete <i class="material-icons right">delete_forever</i>
+        </a>
+        <ul v-if="type!=='explorer'" v-bind:id="targetID" class='dropdown-content'>
+          <li><a>Are you sure?</a></li>
+          <li><a>No</a></li>
+          <li><a v-on:click="deleteProb">Yes</a></li>
+        </ul>
       </div>
     </div>
   </div>  
